3060e7110b7a2d88c71e9d477dbcd9dd3a88506e
jong4876 <jong4876@users.noreply.github.com>
2018-10-10 12:51:43 +0900

    변수명변경

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 var StringBuffer = require("stringbuffer");
-var trafficModule = require('./lib/transportInfoModule.js'); // 모듈단위로 쪼개기
+var tranasPortInfoModule = require('./lib/transportInfoModule.js'); // 모듈단위로 쪼개기
 
 app.get('/', function(req, res) {
   var jsonData = new Object();
-  trafficModule.getInfo(126.9722112, 37.2839068,127.073139, 37.5502596, res);
+  tranasPortInfoModule.getInfo(126.9722112, 37.2839068,127.073139, 37.5502596, res);
 })
 
 
