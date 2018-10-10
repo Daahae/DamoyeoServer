05b9a0a4faf903e96acbc829ff951d0f8f93ac86
jong4876 <jong4876@users.noreply.github.com>
2018-10-11 00:02:21 +0900

    시간지연방식 성공

--- main.js
+++ main.js
@@ -3,14 +3,20 @@ var app = express();
 var request = require('sync-request');
 var transPortInfoModule = require('./lib/transportInfoModule.js'); // 모듈단위로 쪼개기
 var delay = require('delay');
+var bodyParser = require('body-parser');
+var Promise = require('promise');
 
 
 app.get('/', function(req, res) {
   var jsonData = new Object();
-  transPortInfoModule.getInfo(126.9722112, 37.2839068, 127.073139, 37.5502596, res);
 
-  jsonData = transPortInfoModule.getJsonData();
-  res.send(jsonData);
+  transPortInfoModule.getInfo(126.9722112, 37.2839068, 127.073139, 37.5502596);
+
+  setTimeout(function(){
+    jsonData = transPortInfoModule.getInfo();
+    res.send(jsonData);
+  },1000)
+
 })
 
 
